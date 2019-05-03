e9bc89132ffff471b17b7e0353fe9f7428c987ba
mcskypl <mcskypl@gmail.com>
2019-05-04 00:19:53 +0200

    Change .gitignore file.

---
+++ build/precache-manifest.6a6376d6f74d01b3279f2a5e6d2b2bd4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "056476bf54c1813c1d88",
+    "url": "/static/js/main.d2468f7e.chunk.js"
+  },
+  {
+    "revision": "e5b28b348a3897ecbae7",
+    "url": "/static/js/2.e2bdce97.chunk.js"
+  },
+  {
+    "revision": "056476bf54c1813c1d88",
+    "url": "/static/css/main.642bffa8.chunk.css"
+  },
+  {
+    "revision": "0b4b69200a171bb31e848c47876041ef",
+    "url": "/index.html"
+  }
+];
